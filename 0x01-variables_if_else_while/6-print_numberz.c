38656e06a1fd2dfb54dc3b8669ca145049f029c4
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2022-12-12 12:32:27 -0800

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print alphabet
+ * description: print alphabet in order
+ * Return: Always success
+ */
+int main(void)
+{
+int num;
+for (num = 0; num < 10; num++)
+putchar(num + '0');
+putchar('\n');
+return (0);
+}
