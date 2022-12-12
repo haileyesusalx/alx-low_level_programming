c73ad9582b1269cbe97d58cae886db76c362580f
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2022-12-12 09:23:03 -0800

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,10 +16,16 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n < 0)
+	{
 		printf("is negative");
+	}
 	else if (n == 0)
+	{
 		printf("is zero");
+	}
 	else
+	{
 		printf("is positive");
+	}
 	return (0);
 }
