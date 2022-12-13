28c07736e02ce19fa775ee475d8bf8bb9a4a9881
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2022-12-13 15:22:28 -0800

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print letter in alphabet 10 times
+ */
+
+void print_alphabet(void)
+{
+int ten;
+char me;
+for (ten = 0; ten <= 9; ten++)
+{
+	for (me = 'a'; me <= 'z'; me++)
+	_putchar(me);
+_putchar('\n');
+}
+}
