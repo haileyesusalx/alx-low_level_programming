32527aba31d0f3699dd62a9a7ba2edeee17ee178
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2022-12-13 14:18:58 -0800

    task 0

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,7 +1,9 @@
 #include "main.h"
 #include <unistd.h>
+
+
 /**
- * _putchar - writes the character c to std
+ * _putchar - writes the character c to stdout
  * @c: the character to print
  * Return: on success 1
  */
