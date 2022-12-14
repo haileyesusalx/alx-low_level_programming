88c2fdbc18f2cb1d1bb2484fbd7e6c5da74c1513
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2022-12-14 14:46:52 -0800

    task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,17 +7,22 @@
  */
 int main(void)
 {
-	int a = 0, b = 1, next = 0;
-	int sum = 0;
+	int i = 0;
+	long int a = 0, b = 1, next;
 
-	while (next < 4000000)
+	while (i < 50)
 	{
 		next = a + b;
 		a = b;
 		b = next;
-		if (next % 2 == 0)
-			sum += next;
+		printf("%lu", next);
+
+		if (i < 49)
+		{
+			printf(", ");
+		}
+		i++;
 	}
-	printf("%i\n", sum);
+	putchar('\n');
 	return (0);
 }
