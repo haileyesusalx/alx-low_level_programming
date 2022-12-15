286006fd9deaebc7c7213f1fd5e8eea05d78a1bb
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2022-12-15 14:37:30 -0800

    task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
  * print_numbers - print numbers from 0 up t0 9
- * Return: Always 0
+ * Return: 0
  */
 void print_numbers(void)
 {
-	char h = 0;
+	char n = 0;
 
-	while (h <= 9)
+	while (n <= 9)
 	{
-		_putchar('0' + h);
-		h++;
+		_putchar('0' + n);
+		n++;
 	}
 	_putchar('\n');
 }
