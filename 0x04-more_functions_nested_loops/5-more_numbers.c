c8079b2b36714b8d1463e87fc69f27e23a92ccc3
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2022-12-16 13:14:51 -0800

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * more_numbers - prints numbers to 14
+ * Return:0
+ */
+void more_numbers(void)
+{
+int h1, h2;
+for (h1 = 0; h1 < 10; h1++)
+{
+for (h2 = 0; h2 <= 14; h2++)
+{
+if(h2 > 9)
+{
+_putchar((h2 / 10) + '0');
+}
+_putchar((h2 % 10) + '0');
+}
+_putchar(10);
+}
+}
