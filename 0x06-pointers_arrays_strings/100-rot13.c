fceaabc0f7480137ac20aba9b8b1ad10a2935990
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2022-12-22 10:55:20 -0800

    task 8

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -12,17 +12,17 @@ char *rot13(char *s)
 	int j;
 	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
-	
-	for (i = 0; s[i] != '\0'; i++)
+
+for (i = 0; s[i] != '\0'; i++)
+{
+	for (j = 0; j < 52; j++)
 	{
-		for (j = 0; j < 52; j++)
+		if (s[i] == data1[j])
 		{
-			if (s[i] == data1[j])
-			{
-				s[i] = datarot[j];
-				break;
-			}
+			s[i] = datarot[j];
+			break;
 		}
 	}
-	return (s);
+}
+return (s);
 }
