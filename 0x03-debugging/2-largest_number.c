fc165dbd217861ac6b2e699a224f58cd1f03311f
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2022-12-22 15:39:31 -0800

    task 1-3

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,17 +16,20 @@ if (a > b && a > c)
 {
 largest = a;
 }
-else if (b > c && b > a)
-{
-largest = b;
-}
-else if (c > b)
+else if (a > b && c > a)
 {
 largest = c;
 }
+else if (b > c)
+{
+largest = b;
+}
 else
 {
-largest + b;
+largest = c;
 }
+
+
 return (largest);
 }
+
