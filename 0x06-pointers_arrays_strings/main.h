216c74ccb99e94b5c85b9a63dd6416427d58d936
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2022-12-22 08:17:33 -0800

    task 9

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,4 +10,7 @@ char *cap_string(char *str);
 int _putchar(char c);
 char *leet(char *);
 char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 #endif
