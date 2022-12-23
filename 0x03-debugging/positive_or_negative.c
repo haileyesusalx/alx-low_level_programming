c928eeb6b01aea395fa917fdc789270caceca514
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2022-12-22 16:01:16 -0800

    task 0

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,61 @@
+#include <stdio.h>                                                          
+
+#include <stdlib.h>                                                         
+
+#include <time.h>                                                           
+
+                                                                            
+
+/**                                                                         
+ *
+ *  * main - print number value                                                
+ *
+ *   * description: whether number is +ve, -ve or zero                          
+ *
+ *    * Return: 0                                                                
+ *
+ *     */                                                                         
+
+                                                                            
+
+/* function main*/                                                          
+
+int main(void)                                                              
+
+{                                                                           
+
+	        int n;                                                              
+
+		                                                                            
+
+		        srand(time(0));                                                     
+
+			        n = rand() - RAND_MAX / 2;                                          
+
+				        if (n < 0)                                                          
+
+						        {                                                                   
+
+								                printf("%d is negative\n", n);                              
+
+										        }                                                                   
+
+					        else if (n == 0)                                                    
+
+							        {                                                                   
+
+									                printf("%d is zero\n", n);                                  
+
+											        }                                                                   
+
+						        else                                                                
+
+								        {                                                                   
+
+										                printf("%d is positive\n", n);                              
+
+												        }                                                                   
+
+							        return (0);                                                         
+
+}
