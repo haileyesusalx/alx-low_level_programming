fba6b0ce9afb4409ebc6f3daf7c5ddbd166f4ff0
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2023-01-04 22:41:52 -0800

    task 6

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,11 +8,8 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
-int is_prime_number(int n);
-int is_palindrome(char *s);
-int wildcmp(char *s1, char *s2);
-int is_divisible(int num, int div);
-int helperFunction(int num, int i);
 int is_prime_number(int n);
+int is_palindrome(char *s);
+
 #endif
 
