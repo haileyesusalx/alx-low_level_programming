c664a9e186ff0a82133b4695d1f31517e60c8940
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2023-01-22 12:07:07 -0800

    task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,6 @@
-#include "function_pointers.h"
 #include <stdio.h>
+#include "function_pointers.h"
+
 /**
  * array_iterator - prints each array elem on a newl
  *  @array: array
@@ -11,9 +12,8 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (array == NULL || action == NULL)
+	if (!array || !action)
 		return;
 	for (i = 0; i < size; i++)
-	{
 		action(array[i]);
-	}
+}
