bee3f2690255d67761fdb7c6dfdcce6763d7fa37
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2023-03-24 14:31:46 -0700

    task 3

--- 0x18-dynamic_libraries/test.c
+++ 0x18-dynamic_libraries/test.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int printf (const char *__restrict __format, ...)
+int printf(const char *format, ...)
 {
- /* our code goes here 
-* we can use write for printing text
-**/
-write(1, "9 8 10 24 75 - 9\n", 17);
-write(1, "Congratulations, you win the Jackpot!\n", 38);
-exit(EXIT_SUCCESS);
-} 
+	/* our code goes here
+	 * we can use write for printing text
+	 * */
+	write(1, "9 8 10 24 75 - 9\n", 17);
+	write(1, "Congratulations, you win the Jackpot!\n", 38);
+	exit(EXIT_SUCCESS);
+}
