cf23eac9414583fe6bdd44194f615a821f7797cc
haileyesusalx <kefyalewhaileyesusalx@gmail.com>
2023-03-24 06:44:30 -0700

    task 0

---
+++ 0x18-dynamic_libraries/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - write a function that prints a string, followed by anew line,
+ * to stdout.
+ * @str:This is input string
+ */
+void _puts(char *str)
+{
+	int index;
+
+	for (index = 0; str[index] != '\0'; index++)
+	{
+		_putchar(str[index]);
+	}
+	_putchar('\n');
+}
